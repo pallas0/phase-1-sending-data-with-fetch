40c9e91aeee58e290adcee9796ed2be4520284f5
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-01-27 12:34:55 -0800

    commit tree a4ae0d30f49002195f71104ddc6be3464fdc9926

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+
+function submitData(name, email) {
+    const userData = {
+        "name": name,
+        "email": email
+    }
+
+    const configurationObject = {
+       method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(userData)
+    }
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(resp => resp.json())
+    .then(function(object) {
+        const bodyText = document.querySelector('body')
+        const newId = document.createElement('p')
+        newId.textContent = object.id
+        bodyText.appendChild(newId)
+
+    })
+    .catch(function(error) {
+        const newError = document.createElement('p')
+        newError.textContent = error.message
+        const bodyText = document.querySelector('body')
+        bodyText.appendChild(newError)
+    })
+}
